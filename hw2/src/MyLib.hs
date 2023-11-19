6c3bef3e0dba76bbb964bcfcbfe995d63692c71f
Regina Tiapkina <rtapkina@gmail.com>
2023-11-19 10:58:09 +0300

    BST base added

--- hw2/src/MyLib.hs
+++ hw2/src/MyLib.hs
@@ -1,4 +1,43 @@
-module MyLib (someFunc) where
+module MyLib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+rev :: [a] -> [a]
+rev [] = []
+rev (x:xs) = xs -- [x]
+
+-- Binary Search Tree
+--
+-- left < root <= right
+data Tree a
+  = Empty
+  | Node
+    { left :: Maybe (Tree a)
+    , value :: a
+    , right :: Maybe (Tree a)
+    }
+  deriving (Eq,Show,Read)
+
+empty :: Tree a
+empty = Empty
+
+leaf :: a -> Tree a
+leaf a = Node Nothing a Nothing
+
+traversal :: Tree a -> [a]
+traversal Empty = []
+traversal (Node ml v mr)
+  = maybe [] traversal ml ++ [v] ++ maybe [] traversal mr
+
+insert :: Ord a => a -> Tree a -> Tree a
+insert v Empty = leaf v
+insert v t@(Node ml root mr)
+  | v < root  = t{ left = Just $ maybe (leaf v) (insert v) ml }
+  | otherwise = t{ right= Just $ maybe (leaf v) (insert v) mr }
+
+-- Напишите тесты-свойства к функциям и сами функции
+-- левого и правого поворота деревьев
+-- (см. https://en.wikipedia.org/wiki/Red%E2%80%93black_tree)
+rotateLeft :: Tree a -> Tree a
+rotateLeft t = t
+
+rotateRight :: Tree a -> Tree a
+rotateRight t = t
--- hw2/test/Main.hs
+++ hw2/test/Main.hs
@@ -1,4 +1,109 @@
 module Main (main) where
 
+import Test.Tasty
+import Test.Tasty.HUnit hiding (assert)
+import Test.Tasty.Hedgehog
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Data.List (foldl', sort)
+
+import MyLib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain tests
+
+prop_rev :: Property
+prop_rev = property $ do
+  xs <- forAll $ Gen.list (Range.linear 0 10) Gen.alpha
+  rev (rev xs) === xs
+
+prop_opaque :: Property
+prop_opaque = property $ do
+  xs <- forAll $ Gen.list (Range.linear 0 10) Gen.alpha
+  let op = Opaque xs
+  op /== op
+
+prop_div10000 :: Property
+prop_div10000 = property $ do
+  x <- forAll $ Gen.int (Range.linear 1 100000)
+  x `mod` 10000 /== 0
+
+tests :: TestTree
+tests = testGroup "All Tests"
+  [ testGroup "Unit Tests"
+    [ testCase "3*5 == 15" $ 3*5 @?= 15
+    , testCase "2*2 == 4" $ 4 @=? 2*2
+    , testCase "rev []" $ rev [] @?= ([] :: [Int])
+    , testCase "rev [1,2,3]" $
+      rev [1,2,3] @?= [3,2,1]
+    ]
+  , testProperty "reverse works" $ prop_rev
+  , testProperty "strange opaque value" prop_opaque
+  , testProperty "all numbers do not divide 10000" $
+    prop_div10000
+  , treeTests
+  ]
+
+treeTests :: TestTree
+treeTests = testGroup "Tree Tests"
+  [ traversalTests
+  , insertTests
+  ]
+
+traversalTests :: TestTree
+traversalTests = testGroup "traversal"
+  [ testCase "empty" $ traversal empty @?= ([] :: [Int])
+  , testCase "single elt" $ traversal (Node Nothing 1 Nothing) @?= [1]
+  , testCase "three elts" $
+    traversal (Node (Just $ leaf 1) 2 (Just $ leaf 3)) @?= [1,2,3]
+  ]
+
+isCorrect :: Ord a => Tree a -> Bool
+isCorrect Empty = True
+isCorrect (Node ml v mr) = maybe True isCorrect ml &&
+  maybe True isCorrect mr &&
+  all (<v) (maybe [] traversal ml) &&
+  all (>=v) (maybe [] traversal mr)
+
+-- Поменять генератор двоичных деревьев поиска так,
+-- чтобы задавался диапазон элементов дерева и
+-- в левом и в правом поддереве генерировались только
+-- нужные элементы (чтобы BST было корректным)
+arbitraryTree :: Size -> Gen (Tree Int)
+arbitraryTree 0 = pure empty
+arbitraryTree size = do
+  leftSize <- Size <$> Gen.int (Range.linear 0 $ unSize size - 1)
+  let rightSize = size - leftSize - 1
+  l <- if leftSize == 0
+       then pure Nothing
+       else Just <$> arbitraryTree leftSize
+  v <- Gen.int $ Range.linear 0 10000
+  r <- if rightSize == 0
+       then pure Nothing
+       else Just <$> arbitraryTree rightSize
+  pure $ Node l v r
+
+treeGen :: Gen (Tree Int)
+treeGen = Gen.sized arbitraryTree
+
+prop_bst :: Property
+prop_bst = property $ do
+  xs <- forAll $ Gen.list (Range.linear 0 30) $ Gen.int (Range.linear 0 1000)
+  let t = foldl' (\tree elt -> insert elt tree) empty xs
+  assert $ isCorrect t
+
+prop_inserts :: Property
+prop_inserts = property $ do
+  xs <- forAll $ Gen.list (Range.linear 0 30) $ Gen.int (Range.linear 0 1000)
+  let t = foldl' (\tree elt -> insert elt tree) empty xs
+  sort xs === traversal t
+
+insertTests :: TestTree
+insertTests = testGroup "insert"
+  [ testProperty "BST is correct" prop_bst
+  , testProperty "Insert really inserts" prop_inserts
+  , testCase "1" $ isCorrect (leaf 1) @? "leaf 1"
+  , testCase "3" $
+    isCorrect (Node (Just $ leaf 1) 2 (Just $ leaf 2)) @? "tree 3"
+  ]
