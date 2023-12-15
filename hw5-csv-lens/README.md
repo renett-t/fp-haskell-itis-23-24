a15e6ad25819ddd4e02406f29b5db4c23c41808a
Regina Tiapkina <rtapkina@gmail.com>
2023-12-16 01:55:29 +0300

    Run all functions together

--- hw5-csv-lens/README.md
+++ hw5-csv-lens/README.md
@@ -1,1 +1,8 @@
-cabal test --test-options="--match=\"/Main/Album Search Tests\""
+
+Total number of data rows: 30677
+Total number of albums: 28844
+Total number of albums released in 2002: 198
+
+Longest Album from 2002 year with duration 6902775: 
+Name: Ultimate Freestyle Mega Mix
+Release Date: 2008-04-20
--- hw5-csv-lens/app/Main.hs
+++ hw5-csv-lens/app/Main.hs
@@ -8,6 +8,46 @@ import ParsingDataRows
 import SongsFunctions
 
 import Text.Megaparsec (errorBundlePretty)
+import System.IO
+
+
+main :: IO ()
+main = do
+  let filePath = "spotify_songs.csv"
+      delimiter = ','
+      settings = CSVParserSettings delimiter True
+
+  csvFileContent <- readFile filePath
+
+  case parseCSV delimiter settings csvFileContent of
+    Left err -> putStrLn $ "Error parsing CSV:\n" ++ errorBundlePretty err
+    Right csv -> do
+      let dataRows = filterValidDataRows (map dataRowParser (rows csv))
+      -- putStrLn "Data Rows: "
+      -- mapM_ print dataRows
+      putStrLn $ "Total number of data rows: " ++ show (length dataRows)
+
+      let albums = dataRowsToAlbums dataRows
+      -- putStrLn "\nAlbums:"
+      -- mapM_ print albums
+
+      putStrLn $ "Total number of albums: " ++ show (length albums)
+
+      let targetYear = 2002
+      let albumsFromTargetYear = findAlbumsByYear targetYear albums
+      -- putStrLn $ "\nAlbums released in " ++ show targetYear ++ ": "
+      -- mapM_ print albumsFromTargetYear
+
+      putStrLn $ "Total number of albums released in " ++ show targetYear ++ ": " ++ show (length albumsFromTargetYear)
+
+      let longestAlbum = findLongestAlbum albums
+      case longestAlbum of
+        Just album -> do
+          putStrLn $ "\nLongest Album from " ++ show targetYear ++ " year with duration " ++ show (totalAlbumDuration album) ++ ": "
+          putStrLn $ "Name: " ++ _albumName (albumRef album)
+          putStrLn $ "Release Date: " ++ _albumReleaseDate (albumRef album)
+        Nothing ->
+          putStrLn $ "No longest album found for the year " ++ show targetYear
 
 -- main :: IO ()
 -- main = do
