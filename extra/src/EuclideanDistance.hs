96cf95769110d194b931faec9e9c83712c832a61
Regina Tiapkina <rtapkina@gmail.com>
2024-05-15 22:14:22 +0300

    Domain layer added

---
+++ extra/src/EuclideanDistance.hs
@@ -0,0 +1,8 @@
+module EuclideanDistance where
+
+import PubDomain 
+
+-- Calculation of Euclidean distance between two GeoLocation instances
+euclideanDistance :: GeoLocation -> GeoLocation -> Double
+euclideanDistance (GeoLocation lat1 lon1) (GeoLocation lat2 lon2) =
+  sqrt ((lat1 - lat2) ** 2 + (lon1 - lon2) ** 2)
---
+++ extra/src/PubDomain.hs
@@ -0,0 +1,14 @@
+module PubDomain where
+
+data GeoLocation = GeoLocation
+  {
+    lat      :: Double
+  , lon      :: Double
+  } deriving (Show, Eq)
+
+data Pub = Pub
+  {  
+    uid       :: Int
+  , name     :: String
+  , geoLoc   :: GeoLocation
+  } deriving (Show, Eq)
